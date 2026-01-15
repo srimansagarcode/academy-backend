617a6715329e96ac452ff486ffa4a033117d4008
srimansagarcode <srimansagar.code@gmail.com>
2026-01-15 13:46:09 +0530

    Student entity, dto, repository, server impl

---
+++ src/main/java/academy/academy_backend/domain/student/Student.java
@@ -0,0 +1,35 @@
+package academy.academy_backend.domain.student;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    public Student() {} // REQUIRED by JPA & Jackson
+
+    public Long getId(){return id;}
+    public void setId(Long id) {this.id = id;}
+
+    public String getName(){return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getEmail(){return email;}
+    public void setEmail(String email) {this.email = email;}
+
+    public Integer getAge(){ return age;}
+    public void setAge(Integer age) {this.age = age;}
+
+}
